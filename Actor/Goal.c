6dc1f5ca2f6ffc7def1923aa05e6eba3c99c8750
droc101 <37421449+droc101@users.noreply.github.com>
2025-05-31 18:48:09 -0500

    toggleable goal

--- Actor/Goal.c
+++ Actor/Goal.c
@@ -9,6 +9,7 @@
 #include "../Helpers/Collision.h"
 #include "../Helpers/Core/AssetReader.h"
 #include "../Helpers/Core/Error.h"
+#include "../Helpers/Core/KVList.h"
 #include "../Helpers/Core/MathEx.h"
 #include "../Helpers/TextBox.h"
 #include "../Structs/Actor.h"
@@ -19,11 +20,40 @@
 
 #define GOAL_OUTPUT_COLLECTED 2
 
+#define GOAL_INPUT_ENABLE 1
+#define GOAL_INPUT_DISABLE 2
+
+typedef struct GoalData
+{
+	b2ShapeId shapeId;
+	bool enabled;
+} GoalData;
+
+bool GoalSignalHandler(Actor *self, const Actor *sender, byte signal, const Param *param)
+{
+	if (DefaultSignalHandler(self, sender, signal, param))
+	{
+		return true;
+	}
+	GoalData *data = self->extraData;
+	if (signal == GOAL_INPUT_ENABLE && !data->enabled)
+	{
+		data->enabled = true;
+		strcpy(self->actorWall->tex, TEXTURE("actor_goal0"));
+		return true;
+	}
+	if (signal == GOAL_INPUT_DISABLE && data->enabled)
+	{
+		data->enabled = false;
+		strcpy(self->actorWall->tex, TEXTURE("actor_goal1"));
+		return true;
+	}
+	return false;
+}
+
 void CreateGoalSensor(Actor *this, const b2WorldId worldId)
 {
-	this->extraData = calloc(1, sizeof(b2ShapeId));
-	CheckAlloc(this->extraData);
-	b2ShapeId *shapeId = this->extraData;
+	GoalData *goalData = (GoalData *)this->extraData;
 
 	b2BodyDef sensorBodyDef = b2DefaultBodyDef();
 	sensorBodyDef.type = b2_staticBody;
@@ -37,12 +67,18 @@ void CreateGoalSensor(Actor *this, const b2WorldId worldId)
 	sensorShapeDef.isSensor = true;
 	sensorShapeDef.filter.categoryBits = COLLISION_GROUP_ACTOR;
 	sensorShapeDef.filter.maskBits = COLLISION_GROUP_PLAYER;
-	*shapeId = b2CreateCircleShape(this->bodyId, &sensorShapeDef, &sensorShape);
+	goalData->shapeId = b2CreateCircleShape(this->bodyId, &sensorShapeDef, &sensorShape);
 }
 
 void GoalInit(Actor *this, const b2WorldId worldId, KvList *params)
 {
-	this->actorWall = CreateWall(v2(0, 0.5f), v2(0, -0.5f), TEXTURE("actor_goal0"), 1.0f, 0.0f);
+	this->SignalHandler = GoalSignalHandler;
+	this->extraData = calloc(1, sizeof(GoalData));
+	CheckAlloc(this->extraData);
+	GoalData *goalData = (GoalData *)this->extraData;
+	goalData->enabled = KvGetBool(params, "startEnabled", true);
+
+	this->actorWall = CreateWall(v2(0, 0.5f), v2(0, -0.5f), goalData->enabled ? TEXTURE("actor_goal0") : TEXTURE("actor_goal1"), 1.0f, 0.0f);
 	WallBake(this->actorWall);
 
 	CreateGoalSensor(this, worldId);
@@ -50,24 +86,29 @@ void GoalInit(Actor *this, const b2WorldId worldId, KvList *params)
 
 void GoalUpdate(Actor *this, double /*delta*/)
 {
+	const GoalData *goalData = (GoalData *)this->extraData;
+
 	const Vector2 playerPosition = GetState()->level->player.pos;
 	const float rotation = atan2f(playerPosition.y - this->position.y, playerPosition.x - this->position.x) + PIf / 2;
 	this->actorWall->a = v2(0.5f * cosf(rotation), 0.5f * sinf(rotation));
 	this->actorWall->b = v2(-0.5f * cosf(rotation), -0.5f * sinf(rotation));
 
-	if (GetSensorState(GetState()->level->worldId, ((b2ShapeId *)this->extraData)->index1, false))
+	if (goalData->enabled)
 	{
-		const TextBox tb = DEFINE_TEXT("Goal!",
-									   2,
-									   20,
-									   0,
-									   70,
-									   TEXT_BOX_H_ALIGN_CENTER,
-									   TEXT_BOX_V_ALIGN_TOP,
-									   TEXT_BOX_THEME_WHITE);
-		ShowTextBox(tb);
-		ActorFireOutput(this, GOAL_OUTPUT_COLLECTED, PARAM_NONE);
-		RemoveActor(this);
+		if (GetSensorState(GetState()->level->worldId, goalData->shapeId.index1, false))
+		{
+			const TextBox tb = DEFINE_TEXT("Goal!",
+										   2,
+										   20,
+										   0,
+										   70,
+										   TEXT_BOX_H_ALIGN_CENTER,
+										   TEXT_BOX_V_ALIGN_TOP,
+										   TEXT_BOX_THEME_WHITE);
+			ShowTextBox(tb);
+			ActorFireOutput(this, GOAL_OUTPUT_COLLECTED, PARAM_NONE);
+			RemoveActor(this);
+		}
 	}
 }
 
