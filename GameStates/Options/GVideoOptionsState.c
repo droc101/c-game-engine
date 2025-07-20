dc9cf67e429977ed2499705fb4056e8b4e7ed4f5
NBT22 <91098724+NBT22@users.noreply.github.com>
2025-07-19 22:57:48 -0500

    No more wip

--- GameStates/Options/GVideoOptionsState.c
+++ GameStates/Options/GVideoOptionsState.c
@@ -181,7 +181,7 @@ void GVideoOptionsStateSet()
 		UiStackPush(videoOptionsStack,
 					CreateRadioButtonControl(v2(0, opY),
 											 v2(480, 40),
-											 "Vulkan (WIP)",
+											 "Vulkan",
 											 RbOptionsRenderer,
 											 TOP_CENTER,
 											 GetState()->options.renderer == RENDERER_VULKAN,
