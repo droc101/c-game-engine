2fabffb8f43504c7d5bbd56e0940a235761b5a72
droc101 <37421449+droc101@users.noreply.github.com>
2025-09-04 09:09:27 -0500

    Make GLoadingState have a minimum display time instead of an additional display time (the min is 250ms, same as the previous additional time)

--- GameStates/GLoadingState.c
+++ GameStates/GLoadingState.c
@@ -4,9 +4,11 @@
 
 #include "GLoadingState.h"
 #include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 #include "../Helpers/Core/Logging.h"
 #include "../Helpers/Core/SoundSystem.h"
+#include "../Helpers/Core/Timing.h"
 #include "../Helpers/Graphics/Font.h"
 #include "../Helpers/Graphics/RenderingHelpers.h"
 #include "../Structs/Color.h"
@@ -14,19 +16,26 @@
 #include "../Structs/Vector2.h"
 #include "GMainState.h"
 
+/// The minimum time the loading screen should be visible for, to prevent quick flashes
+#define LEVEL_LOAD_MIN_TIME_MS 250
+
 char loadStateLevelname[32];
+uint64_t levelLoadStartTime;
 
 void GLoadingStateUpdate(GlobalState *state)
 {
-	if (state->physicsFrame >= 15)
+	if (state->physicsFrame == 1)
 	{
-		if (ChangeLevelByName((char *)&loadStateLevelname))
-		{
-			GMainStateSet();
-		} else
+		if (!ChangeLevelByName((char *)&loadStateLevelname))
 		{
 			LogError("Failed to load level: %s\n", loadStateLevelname);
 		}
+	}
+	const uint64_t currentTime = GetTimeMs();
+	const uint64_t loadTime = currentTime - levelLoadStartTime;
+	if (loadTime > LEVEL_LOAD_MIN_TIME_MS)
+	{
+		GMainStateSet();
 	}
 }
 
@@ -44,6 +53,7 @@ void GLoadingStateRender(GlobalState * /*state*/)
 
 void GLoadingStateSet(const char *levelName)
 {
+	levelLoadStartTime = GetTimeMs();
 	strncpy(loadStateLevelname, levelName, 31);
 	StopMusic();
 	SetStateCallbacks(GLoadingStateUpdate, NULL, LOADING_STATE, GLoadingStateRender);
