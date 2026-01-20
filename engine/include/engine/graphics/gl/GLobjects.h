droc101/c-game-engine

commit 42feadc9e1e85e7d8da4de30df6441ab992f2c3d

OpenGL render to internal framebuffer of specific format then copy to window framebuffer as given by SDL

engine/include/engine/graphics/gl/GLshaders.h
@@ -25,8 +25,17 @@ extern GL_Shader *modelUnshadedShader;
 extern GL_Shader *modelShadedShader;
 /// The shader used for the debug renderer
 extern GL_Shader *debugShader;
+/// The shader used to copy the internal framebuffer to the window
+extern GL_Shader *framebufferShader;
 
 #pragma region Uniform/Attribute Locations
+
+/// The location of the vertex position attribute in the framebuffer shader
+extern GLuint framebufferPositionLoc;
+/// The location of the vertex UV attribute in the framebuffer shader
+extern GLuint framebufferUvLoc;
+/// The location of the texture uniform in the framebuffer shader
+extern GLint framebufferTextureLoc;
 
 /// The location of the color uniform in the UI colored shader
 extern GLint uiColoredColorLoc;

engine/src/graphics/gl/GLframe.c
@@ -6,14 +6,137 @@
 #include <engine/graphics/gl/GLdebug.h>
 #include <engine/graphics/gl/GLframe.h>
 #include <engine/graphics/gl/GLobjects.h>
+#include <engine/graphics/gl/GLshaders.h>
 #include <engine/graphics/RenderingHelpers.h>
+#include <engine/subsystem/Logging.h>
 #include <SDL_video.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
+
+#define GL_COLOR_INTERNAL_FORMAT GL_RGB8
+#define GL_COLOR_FORMAT GL_RGB
+#define GL_COLOR_TYPE GL_UNSIGNED_BYTE
+#define GL_DEPTH_FORMAT GL_DEPTH24_STENCIL8
+
+GLuint frameBufferObject;
+GLuint renderBufferObject;
+GLuint framebufferColorTexture;
+GL_Buffer *fullScreenQuadBuffer;
+
+bool GL_InitFramebuffer()
+{
+	glGenFramebuffers(1, &frameBufferObject);
+	glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObject);
+	glGenRenderbuffers(1, &renderBufferObject);
+	glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
+
+	glGenTextures(1, &framebufferColorTexture);
+	glBindTexture(GL_TEXTURE_2D, framebufferColorTexture);
+
+	glTexImage2D(GL_TEXTURE_2D,
+				 0,
+				 GL_COLOR_INTERNAL_FORMAT,
+				 DEF_WIDTH,
+				 DEF_HEIGHT,
+				 0,
+				 GL_COLOR_FORMAT,
+				 GL_COLOR_TYPE,
+				 NULL);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferColorTexture, 0);
+
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_FORMAT, DEF_WIDTH, DEF_WIDTH);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderBufferObject);
+
+	const GLenum frameBufferStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	if (frameBufferStatus != GL_FRAMEBUFFER_COMPLETE)
+	{
+		LogError("OpenGL framebuffer is incomplete, status is %u\n", frameBufferStatus);
+		return false;
+	}
+
+#ifdef BUILDSTYLE_DEBUG
+	int redSize, greenSize, blueSize, alphaSize, depthSize, stencilSize;
+	glGetFramebufferAttachmentParameteriv(GL_FRAMEBUFFER,
+										  GL_COLOR_ATTACHMENT0,
+										  GL_FRAMEBUFFER_ATTACHMENT_RED_SIZE,
+										  &redSize);
+	glGetFramebufferAttachmentParameteriv(GL_FRAMEBUFFER,
+										  GL_COLOR_ATTACHMENT0,
+										  GL_FRAMEBUFFER_ATTACHMENT_GREEN_SIZE,
+										  &greenSize);
+	glGetFramebufferAttachmentParameteriv(GL_FRAMEBUFFER,
+										  GL_COLOR_ATTACHMENT0,
+										  GL_FRAMEBUFFER_ATTACHMENT_BLUE_SIZE,
+										  &blueSize);
+	glGetFramebufferAttachmentParameteriv(GL_FRAMEBUFFER,
+										  GL_COLOR_ATTACHMENT0,
+										  GL_FRAMEBUFFER_ATTACHMENT_ALPHA_SIZE,
+										  &alphaSize);
+	glGetFramebufferAttachmentParameteriv(GL_FRAMEBUFFER,
+										  GL_DEPTH_ATTACHMENT,
+										  GL_FRAMEBUFFER_ATTACHMENT_DEPTH_SIZE,
+										  &depthSize);
+	glGetFramebufferAttachmentParameteriv(GL_FRAMEBUFFER,
+										  GL_DEPTH_ATTACHMENT,
+										  GL_FRAMEBUFFER_ATTACHMENT_STENCIL_SIZE,
+										  &stencilSize);
+	LogDebug("Internal Framebuffer: R:%d G:%d B:%d A:%d D:%d S:%d\n",
+			 redSize,
+			 greenSize,
+			 blueSize,
+			 alphaSize,
+			 depthSize,
+			 stencilSize);
+#endif
+
+	glBindFramebuffer(GL_FRAMEBUFFER, GL_NONE);
+	glBindRenderbuffer(GL_RENDERBUFFER, GL_NONE);
+
+	fullScreenQuadBuffer = GL_ConstructBuffer();
+	GL_UseShader(framebufferShader);
+
+	const float vertices[4][4] = {
+		{-1, 1, 0, 1},
+		{-1, -1, 0, 0},
+		{1, -1, 1, 0},
+		{1, 1, 1, 1},
+	};
+
+	const uint32_t indices[] = {0, 1, 2, 0, 2, 3};
+
+	GL_BindBuffer(fullScreenQuadBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STREAM_DRAW);
+
+	glVertexAttribPointer(framebufferPositionLoc, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void *)0);
+	glEnableVertexAttribArray(framebufferPositionLoc);
+
+	glVertexAttribPointer(framebufferUvLoc, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void *)(2 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(framebufferUvLoc);
+
+	return true;
+}
+
+void GL_DestroyFramebuffer()
+{
+	glDeleteFramebuffers(1, &frameBufferObject);
+	glDeleteRenderbuffers(1, &renderBufferObject);
+	glDeleteTextures(1, &framebufferColorTexture);
+	GL_DestroyBuffer(fullScreenQuadBuffer);
+}
 
 bool GL_FrameStart()
 {
 	GL_ResetDebugLines();
+	glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObject);
+	glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
 	return true;
 }
 
@@ -27,8 +150,19 @@ inline void GL_ClearDepthOnly()
 	glClear(GL_DEPTH_BUFFER_BIT);
 }
 
-inline void GL_Swap()
+inline void GL_FrameEnd()
 {
+	glBindFramebuffer(GL_FRAMEBUFFER, GL_NONE);
+	glBindRenderbuffer(GL_RENDERBUFFER, GL_NONE);
+
+	glDisable(GL_DEPTH_TEST);
+	GL_UseShader(framebufferShader);
+	GL_BindBuffer(fullScreenQuadBuffer);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, framebufferColorTexture);
+	glUniform1i(framebufferTextureLoc, 0);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+
 	SDL_GL_SwapWindow(GetGameWindow());
 }
 
@@ -38,4 +172,21 @@ inline void GL_UpdateViewportSize()
 	int vpHeight = 0;
 	SDL_GL_GetDrawableSize(GetGameWindow(), &vpWidth, &vpHeight);
 	glViewport(0, 0, vpWidth, vpHeight);
+
+	glBindTexture(GL_TEXTURE_2D, framebufferColorTexture);
+	glTexImage2D(GL_TEXTURE_2D,
+				 0,
+				 GL_COLOR_INTERNAL_FORMAT,
+				 vpWidth,
+				 vpHeight,
+				 0,
+				 GL_COLOR_FORMAT,
+				 GL_COLOR_TYPE, NULL);
+
+	GLint boundRbo = 0;
+	glGetIntegerv(GL_RENDERBUFFER_BINDING, &boundRbo);
+
+	glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_FORMAT, vpWidth, vpHeight);
+	glBindRenderbuffer(GL_RENDERBUFFER, boundRbo);
 }

engine/src/graphics/gl/GLinit.c
@@ -3,6 +3,7 @@
 //
 
 #include <engine/graphics/gl/GLdebug.h>
+#include <engine/graphics/gl/GLframe.h>
 #include <engine/graphics/gl/GLinit.h>
 #include <engine/graphics/gl/GLobjects.h>
 #include <engine/graphics/gl/GLshaders.h>
@@ -68,13 +69,7 @@ bool GL_PreInit()
 	TestSDLFunction(SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1),
 					"Failed to set OpenGL double buffer",
 					GL_INIT_FAIL_MSG);
-	TestSDLFunction(SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8), "Failed to set OpenGL red-size", "Failed to start OpenGL");
-	TestSDLFunction(SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8), "Failed to set OpenGL green-size", GL_INIT_FAIL_MSG);
-	TestSDLFunction(SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8), "Failed to set OpenGL blue-size", GL_INIT_FAIL_MSG);
-	TestSDLFunction(SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8), "Failed to set OpenGL alpha-size", GL_INIT_FAIL_MSG);
-	TestSDLFunction(SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24),
-					"Failed to set OpenGL depth buffer size",
-					GL_INIT_FAIL_MSG);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
 
 	return true;
 }
@@ -158,6 +153,13 @@ bool GL_Init(SDL_Window *wnd)
 #ifdef BUILDSTYLE_DEBUG
 	glEnable(GL_DEBUG_OUTPUT);
 	glDebugMessageCallback(GL_DebugMessageCallback, NULL);
+
+	int redSize, greenSize, blueSize, alphaSize;
+	SDL_GL_GetAttribute(SDL_GL_RED_SIZE, &redSize);
+	SDL_GL_GetAttribute(SDL_GL_GREEN_SIZE, &greenSize);
+	SDL_GL_GetAttribute(SDL_GL_BLUE_SIZE, &blueSize);
+	SDL_GL_GetAttribute(SDL_GL_ALPHA_SIZE, &alphaSize);
+	LogDebug("Window Framebuffer: R:%d G:%d B:%d A:%d\n", redSize, greenSize, blueSize, alphaSize);
 #endif
 
 	if (!GL_LoadShaders())
@@ -166,6 +168,11 @@ bool GL_Init(SDL_Window *wnd)
 	}
 
 	GL_InitObjects();
+
+	if (!GL_InitFramebuffer())
+	{
+		return false;
+	}
 
 	glClearColor(0, 0, 0, 1);
 
@@ -196,6 +203,7 @@ bool GL_Init(SDL_Window *wnd)
 void GL_DestroyGL()
 {
 	LogDebug("Cleaning up OpenGL renderer...\n");
+	GL_DestroyFramebuffer();
 	GL_DestroyShaders();
 	GL_DestroyObjects();
 	SDL_GL_DeleteContext(ctx);

engine/src/graphics/gl/GLshaders.c
@@ -18,6 +18,11 @@ GL_Shader *skyShader;
 GL_Shader *modelUnshadedShader;
 GL_Shader *modelShadedShader;
 GL_Shader *debugShader;
+GL_Shader *framebufferShader;
+
+GLuint framebufferPositionLoc;
+GLuint framebufferUvLoc;
+GLint framebufferTextureLoc;
 
 GLint uiColoredColorLoc;
 GLint uiColoredVertexLoc;
@@ -82,6 +87,7 @@ bool GL_LoadShaders()
 	modelShadedShader = GL_ConstructShaderFromAssets(SHADER("gl/model_shaded_f"), SHADER("gl/model_shaded_v"));
 	modelUnshadedShader = GL_ConstructShaderFromAssets(SHADER("gl/model_unshaded_f"), SHADER("gl/model_unshaded_v"));
 	debugShader = GL_ConstructShaderFromAssets(SHADER("gl/debug_f"), SHADER("gl/debug_v"));
+	framebufferShader = GL_ConstructShaderFromAssets(SHADER("gl/framebuffer_f"), SHADER("gl/framebuffer_v"));
 
 	if (!uiTexturedShader ||
 		!uiColoredShader ||
@@ -90,11 +96,16 @@ bool GL_LoadShaders()
 		!skyShader ||
 		!modelShadedShader ||
 		!modelUnshadedShader ||
-		!debugShader)
+		!debugShader ||
+		!framebufferShader)
 	{
 		GL_Error("Failed to compile shaders");
 		return false;
 	}
+
+	framebufferPositionLoc = glGetAttribLocation(framebufferShader->program, "VERTEX");
+	framebufferUvLoc = glGetAttribLocation(framebufferShader->program, "VERTEX_UV");
+	framebufferTextureLoc = glGetUniformLocation(framebufferShader->program, "screenTexture");
 
 	uiColoredColorLoc = glGetUniformLocation(uiColoredShader->program, "col");
 	uiColoredVertexLoc = glGetAttribLocation(uiColoredShader->program, "VERTEX");

engine/include/engine/graphics/gl/GLframe.h
@@ -9,6 +9,16 @@
 #include <stdbool.h>
 
 /**
+ * Create the OpenGL framebuffer and related data
+ */
+bool GL_InitFramebuffer();
+
+/**
+ * Destroy the OpenGL framebuffer and related data
+ */
+void GL_DestroyFramebuffer();
+
+/**
  * Performs start of frame tasks
  */
 bool GL_FrameStart();
@@ -24,12 +34,12 @@ void GL_ClearScreen();
 void GL_ClearDepthOnly();
 
 /**
- * Swap the buffers
+ * Copy the internal framebuffer and swap buffers
  */
-void GL_Swap();
+void GL_FrameEnd();
 
 /**
- * Update the viewport size
+ * Update the viewport and framebuffer sizes
  */
 void GL_UpdateViewportSize();
 

engine/src/graphics/RenderingHelpers.c
@@ -177,7 +177,7 @@ void FrameEnd()
 			VK_FrameEnd();
 			break;
 		case RENDERER_OPENGL:
-			GL_Swap();
+			GL_FrameEnd();
 			break;
 		default:
 			break;

commit 1f5308c92840400e92171b10e2e3d05862e58e2f

Fix OpenGL MSAA that was broken with commit 42feadc

engine/include/engine/graphics/gl/GLobjects.h
@@ -110,6 +110,9 @@ extern GLuint sharedUniformBuffer;
 /// Texture anisotropy level
 extern GLfloat anisotropyLevel;
 
+/// Number of MSAA samples
+extern GLint glMsaaSamples;
+
 /**
  * Create a shader program from assets
  * @param fsh The fragment shader asset

engine/include/engine/graphics/gl/GLshaders.h
@@ -25,17 +25,8 @@ extern GL_Shader *modelUnshadedShader;
 extern GL_Shader *modelShadedShader;
 /// The shader used for the debug renderer
 extern GL_Shader *debugShader;
-/// The shader used to copy the internal framebuffer to the window
-extern GL_Shader *framebufferShader;
 
 #pragma region Uniform/Attribute Locations
-
-/// The location of the vertex position attribute in the framebuffer shader
-extern GLuint framebufferPositionLoc;
-/// The location of the vertex UV attribute in the framebuffer shader
-extern GLuint framebufferUvLoc;
-/// The location of the texture uniform in the framebuffer shader
-extern GLint framebufferTextureLoc;
 
 /// The location of the color uniform in the UI colored shader
 extern GLint uiColoredColorLoc;

engine/src/graphics/gl/GLframe.c
@@ -22,7 +22,6 @@
 GLuint frameBufferObject;
 GLuint renderBufferObject;
 GLuint framebufferColorTexture;
-GL_Buffer *fullScreenQuadBuffer;
 
 bool GL_InitFramebuffer()
 {
@@ -32,27 +31,53 @@ bool GL_InitFramebuffer()
 	glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
 
 	glGenTextures(1, &framebufferColorTexture);
-	glBindTexture(GL_TEXTURE_2D, framebufferColorTexture);
+	if (glMsaaSamples != 0)
+	{
+		glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, framebufferColorTexture);
 
-	glTexImage2D(GL_TEXTURE_2D,
-				 0,
-				 GL_COLOR_INTERNAL_FORMAT,
-				 DEF_WIDTH,
-				 DEF_HEIGHT,
-				 0,
-				 GL_COLOR_FORMAT,
-				 GL_COLOR_TYPE,
-				 NULL);
+		glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE,
+								glMsaaSamples,
+								GL_COLOR_INTERNAL_FORMAT,
+								DEF_WIDTH,
+								DEF_HEIGHT,
+								GL_TRUE);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		// Multisample textures do not have filter or repeat,
+		// and it doesn't matter here anyway since we just blit it to the main buffer
 
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferColorTexture, 0);
+		glFramebufferTexture2D(GL_FRAMEBUFFER,
+							   GL_COLOR_ATTACHMENT0,
+							   GL_TEXTURE_2D_MULTISAMPLE,
+							   framebufferColorTexture,
+							   0);
 
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_FORMAT, DEF_WIDTH, DEF_WIDTH);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderBufferObject);
+		glRenderbufferStorageMultisample(GL_RENDERBUFFER, glMsaaSamples, GL_DEPTH_FORMAT, DEF_WIDTH, DEF_WIDTH);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderBufferObject);
+	} else
+	{
+		glBindTexture(GL_TEXTURE_2D, framebufferColorTexture);
+
+		glTexImage2D(GL_TEXTURE_2D,
+					 0,
+					 GL_COLOR_INTERNAL_FORMAT,
+					 DEF_WIDTH,
+					 DEF_HEIGHT,
+					 0,
+					 GL_COLOR_FORMAT,
+					 GL_COLOR_TYPE,
+					 NULL);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferColorTexture, 0);
+
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_FORMAT, DEF_WIDTH, DEF_WIDTH);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderBufferObject);
+	}
+
 
 	const GLenum frameBufferStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 	if (frameBufferStatus != GL_FRAMEBUFFER_COMPLETE)
@@ -99,27 +124,7 @@ bool GL_InitFramebuffer()
 	glBindFramebuffer(GL_FRAMEBUFFER, GL_NONE);
 	glBindRenderbuffer(GL_RENDERBUFFER, GL_NONE);
 
-	fullScreenQuadBuffer = GL_ConstructBuffer();
-	GL_UseShader(framebufferShader);
-
-	const float vertices[4][4] = {
-		{-1, 1, 0, 1},
-		{-1, -1, 0, 0},
-		{1, -1, 1, 0},
-		{1, 1, 1, 1},
-	};
-
 	const uint32_t indices[] = {0, 1, 2, 0, 2, 3};
-
-	GL_BindBuffer(fullScreenQuadBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STREAM_DRAW);
-
-	glVertexAttribPointer(framebufferPositionLoc, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void *)0);
-	glEnableVertexAttribArray(framebufferPositionLoc);
-
-	glVertexAttribPointer(framebufferUvLoc, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void *)(2 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(framebufferUvLoc);
 
 	return true;
 }
@@ -129,7 +134,6 @@ void GL_DestroyFramebuffer()
 	glDeleteFramebuffers(1, &frameBufferObject);
 	glDeleteRenderbuffers(1, &renderBufferObject);
 	glDeleteTextures(1, &framebufferColorTexture);
-	GL_DestroyBuffer(fullScreenQuadBuffer);
 }
 
 bool GL_FrameStart()
@@ -152,16 +156,19 @@ inline void GL_ClearDepthOnly()
 
 inline void GL_FrameEnd()
 {
-	glBindFramebuffer(GL_FRAMEBUFFER, GL_NONE);
-	glBindRenderbuffer(GL_RENDERBUFFER, GL_NONE);
-
-	glDisable(GL_DEPTH_TEST);
-	GL_UseShader(framebufferShader);
-	GL_BindBuffer(fullScreenQuadBuffer);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, framebufferColorTexture);
-	glUniform1i(framebufferTextureLoc, 0);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+	const Vector2 wndSize = ActualWindowSize();
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, frameBufferObject);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, GL_NONE);
+	glBlitFramebuffer(0,
+					  0,
+					  (GLint)wndSize.x,
+					  (GLint)wndSize.y,
+					  0,
+					  0,
+					  (GLint)wndSize.x,
+					  (GLint)wndSize.y,
+					  GL_COLOR_BUFFER_BIT,
+					  GL_NEAREST);
 
 	SDL_GL_SwapWindow(GetGameWindow());
 }
@@ -173,20 +180,35 @@ inline void GL_UpdateViewportSize()
 	SDL_GL_GetDrawableSize(GetGameWindow(), &vpWidth, &vpHeight);
 	glViewport(0, 0, vpWidth, vpHeight);
 
-	glBindTexture(GL_TEXTURE_2D, framebufferColorTexture);
-	glTexImage2D(GL_TEXTURE_2D,
-				 0,
-				 GL_COLOR_INTERNAL_FORMAT,
-				 vpWidth,
-				 vpHeight,
-				 0,
-				 GL_COLOR_FORMAT,
-				 GL_COLOR_TYPE, NULL);
-
 	GLint boundRbo = 0;
 	glGetIntegerv(GL_RENDERBUFFER_BINDING, &boundRbo);
-
 	glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_FORMAT, vpWidth, vpHeight);
+
+	if (glMsaaSamples != 0)
+	{
+		glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, framebufferColorTexture);
+		glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE,
+								glMsaaSamples,
+								GL_COLOR_INTERNAL_FORMAT,
+								vpWidth,
+								vpHeight,
+								GL_TRUE);
+
+		glRenderbufferStorageMultisample(GL_RENDERBUFFER, glMsaaSamples, GL_DEPTH_FORMAT, vpWidth, vpHeight);
+	} else
+	{
+		glBindTexture(GL_TEXTURE_2D, framebufferColorTexture);
+		glTexImage2D(GL_TEXTURE_2D,
+					 0,
+					 GL_COLOR_INTERNAL_FORMAT,
+					 vpWidth,
+					 vpHeight,
+					 0,
+					 GL_COLOR_FORMAT,
+					 GL_COLOR_TYPE,
+					 NULL);
+
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_FORMAT, vpWidth, vpHeight);
+	}
 	glBindRenderbuffer(GL_RENDERBUFFER, boundRbo);
 }

engine/src/graphics/gl/GLinit.c
@@ -49,11 +49,14 @@ bool GL_PreInit()
 				LogError("OpenGL: Invalid MSAA value!");
 				return false;
 		}
-		if (SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, mssaValue) != 0)
-		{
-			LogError("Failed to set MSAA samples attribute: %s\n", SDL_GetError());
-		}
+		glMsaaSamples = mssaValue;
 	}
+	TestSDLFunction(SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 0),
+					"Failed to set OpenGL MSAA buffers",
+					GL_INIT_FAIL_MSG);
+	TestSDLFunction(SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0),
+					"Failed to set OpenGL MSAA samples",
+					GL_INIT_FAIL_MSG);
 	TestSDLFunction(SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, GL_VERSION_MAJOR),
 					"Failed to set OpenGL major version",
 					GL_INIT_FAIL_MSG);

engine/src/graphics/gl/GLobjects.c
@@ -31,6 +31,8 @@ GLuint sharedUniformBuffer;
 
 GLfloat anisotropyLevel = 0;
 
+GLint glMsaaSamples = 0;
+
 GL_Shader *GL_ConstructShaderFromAssets(const char *fsh, const char *vsh)
 {
 	Shader *fragmentSource = LoadShader(fsh);

engine/src/graphics/gl/GLshaders.c
@@ -19,11 +19,6 @@ GL_Shader *skyShader;
 GL_Shader *modelUnshadedShader;
 GL_Shader *modelShadedShader;
 GL_Shader *debugShader;
-GL_Shader *framebufferShader;
-
-GLuint framebufferPositionLoc;
-GLuint framebufferUvLoc;
-GLint framebufferTextureLoc;
 
 GLint uiColoredColorLoc;
 GLint uiColoredVertexLoc;
@@ -88,7 +83,6 @@ bool GL_LoadShaders()
 	modelShadedShader = GL_ConstructShaderFromAssets(SHADER("gl/model_shaded_f"), SHADER("gl/model_shaded_v"));
 	modelUnshadedShader = GL_ConstructShaderFromAssets(SHADER("gl/model_unshaded_f"), SHADER("gl/model_unshaded_v"));
 	debugShader = GL_ConstructShaderFromAssets(SHADER("gl/debug_f"), SHADER("gl/debug_v"));
-	framebufferShader = GL_ConstructShaderFromAssets(SHADER("gl/framebuffer_f"), SHADER("gl/framebuffer_v"));
 
 	if (!uiTexturedShader ||
 		!uiColoredShader ||
@@ -97,16 +91,11 @@ bool GL_LoadShaders()
 		!skyShader ||
 		!modelShadedShader ||
 		!modelUnshadedShader ||
-		!debugShader ||
-		!framebufferShader)
+		!debugShader)
 	{
 		LogError("OpenGL: Failed to compile shaders");
 		return false;
 	}
-
-	framebufferPositionLoc = glGetAttribLocation(framebufferShader->program, "VERTEX");
-	framebufferUvLoc = glGetAttribLocation(framebufferShader->program, "VERTEX_UV");
-	framebufferTextureLoc = glGetUniformLocation(framebufferShader->program, "screenTexture");
 
 	uiColoredColorLoc = glGetUniformLocation(uiColoredShader->program, "col");
 	uiColoredVertexLoc = glGetAttribLocation(uiColoredShader->program, "VERTEX");