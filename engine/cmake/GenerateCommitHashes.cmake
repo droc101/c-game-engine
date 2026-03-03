e4f42099f8577f676c1560ed15e0595a094ca503
droc101 <37421449+droc101@users.noreply.github.com>
2026-03-02 21:34:40 -0600

    Fix git hash on linux actions builds

--- engine/cmake/GenerateCommitHashes.cmake
+++ engine/cmake/GenerateCommitHashes.cmake
@@ -1,37 +1,37 @@
-find_package(Git 2.18 REQUIRED QUIET)
-
 set(GIT_SHORT_HASH "unknown")
 set(GIT_HASH "unknown")
+set(GIT_EXECUTABLE "git") # find_package(Git) fails here for some reason
 
-if (${GIT_FOUND})
+execute_process(
+        COMMAND ${GIT_EXECUTABLE} status --short --porcelain=1 --untracked-files=no
+        WORKING_DIRECTORY ${REPO_DIR}
+        OUTPUT_VARIABLE GIT_STATUS
+        RESULT_VARIABLE GIT_STATUS_RETURN_CODE
+        ERROR_VARIABLE GIT_STATUS_ERROR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+if (${GIT_STATUS_RETURN_CODE} EQUAL 0)
     execute_process(
-            COMMAND ${GIT_EXECUTABLE} status --short --porcelain=1 --untracked-files=no
+            COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
             WORKING_DIRECTORY ${REPO_DIR}
-            OUTPUT_VARIABLE GIT_STATUS
-            RESULT_VARIABLE GIT_STATUS_RETURN_CODE
+            OUTPUT_VARIABLE GIT_SHORT_HASH
             OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
-    if (${GIT_STATUS_RETURN_CODE} EQUAL 0)
-        execute_process(
-                COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-                WORKING_DIRECTORY ${REPO_DIR}
-                OUTPUT_VARIABLE GIT_SHORT_HASH
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
+    execute_process(
+            COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+            WORKING_DIRECTORY ${REPO_DIR}
+            OUTPUT_VARIABLE GIT_HASH
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
 
-        execute_process(
-                COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
-                WORKING_DIRECTORY ${REPO_DIR}
-                OUTPUT_VARIABLE GIT_HASH
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-
-        if (NOT "${GIT_STATUS}" STREQUAL "")
-            set(GIT_SHORT_HASH "based on ${GIT_SHORT_HASH}")
-            set(GIT_HASH "uncommitted changes based on ${GIT_HASH}")
-        endif ()
+    if (NOT "${GIT_STATUS}" STREQUAL "")
+        set(GIT_SHORT_HASH "based on ${GIT_SHORT_HASH}")
+        set(GIT_HASH "uncommitted changes based on ${GIT_HASH}")
     endif ()
+else ()
+    message(WARNING "git status failed with code ${GIT_STATUS_RETURN_CODE}! stdout: ${GIT_STATUS} stderr: ${GIT_STATUS_ERROR}")
 endif ()
 
 message(STATUS "Git hash: ${GIT_HASH}")
