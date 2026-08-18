98c10b5885f3b08d03816db1065d7da4d8aaebaa
droc101 <37421449+droc101@users.noreply.github.com>
2026-08-17 22:06:16 -0500

    dont draw menu background texture in base options screen and map select

--- game/src/gameState/LevelSelectState.c
+++ game/src/gameState/LevelSelectState.c
@@ -67,7 +67,7 @@ static void LevelSelectStateUpdate(GlobalState *state, const double delta)
 
 static void LevelSelectStateRender(GlobalState *state, const double /*delta*/)
 {
-	RenderMenuBackground(state, true);
+	RenderMenuBackground(state, false);
 	if (!IsBackgroundMapLoaded())
 	{
 		return;
