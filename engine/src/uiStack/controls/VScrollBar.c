4060f89fa172efc6356af9fd75f32141431acfce
droc101 <37421449+droc101@users.noreply.github.com>
2026-08-17 21:49:22 -0500

    oops i forgot to re enable the code i disabled for debugging

--- engine/src/uiStack/controls/VScrollBar.c
+++ engine/src/uiStack/controls/VScrollBar.c
@@ -32,7 +32,6 @@ void DestroyVScrollBar(const Control *) {}
 
 void UpdateVScrollBar(UiStack *stack, Control *c, Vector2 /*localMousePos*/, uint32_t ctlIndex)
 {
-	return;
 	VScrollBarData *data = (VScrollBarData *)c->controlData;
 	if (stack->activeControl == ctlIndex)
 	{
@@ -58,7 +57,6 @@ void UpdateVScrollBar(UiStack *stack, Control *c, Vector2 /*localMousePos*/, uin
 
 void AlwaysUpdateVScrollBar(UiStack * /*stack*/, Control *c, Vector2 /*localMousePos*/, uint32_t /*ctlIndex*/)
 {
-	return;
 	VScrollBarData *data = (VScrollBarData *)c->controlData;
 	c->allowFocus = !UseController(mainThreadInput) && data->contentHeight > c->size.y;
 	if (data->contentHeight > c->size.y)
