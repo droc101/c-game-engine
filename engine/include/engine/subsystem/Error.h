774f8a1e38137e99831760beea53c3f4b5998830
droc101 <37421449+droc101@users.noreply.github.com>
2026-09-10 11:30:34 -0500

    Don't include full path to source tree in __FILE__ macro

--- engine/include/engine/subsystem/Error.h
+++ engine/include/engine/subsystem/Error.h
@@ -8,7 +8,7 @@
 #include <stdbool.h>
 
 /// Throw a fatal error
-#define Error(error) _ErrorInternal(error, __FILE_NAME__, __LINE__, __PRETTY_FUNCTION__) // NOLINT(*-include-cleaner)
+#define Error(error) _ErrorInternal(error, __FILE__, __LINE__, __PRETTY_FUNCTION__) // NOLINT(*-include-cleaner)
 
 /**
  * Check if a pointer is NULL and if it is, call the error handler
