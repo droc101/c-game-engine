b2975d189d9c59452d2212ebd4257e368ab915b8
NBT22 <91098724+NBT22@users.noreply.github.com>
2026-09-10 16:58:09 -0500

    Use preprocessor defines for logging colors

--- engine/include/engine/subsystem/Logging.h
+++ engine/include/engine/subsystem/Logging.h
@@ -12,6 +12,19 @@
 #define FLUSH_ON_WARNING true
 #define FLUSH_ON_ERROR true
 
+#ifndef LOGGER_INFO_COLOR
+#define LOGGER_INFO_COLOR 39
+#endif
+#ifndef LOGGER_DEBUG_COLOR
+#define LOGGER_DEBUG_COLOR 39
+#endif
+#ifndef LOGGER_WARNING_COLOR
+#define LOGGER_WARNING_COLOR 33
+#endif
+#ifndef LOGGER_ERROR_COLOR
+#define LOGGER_ERROR_COLOR 31
+#endif
+
 /**
  * Initialize the logging system
  */
@@ -36,14 +49,14 @@ void LogInternal(const char *type, int color, bool flush, const char *message, .
  * Log an info message
  * @param ... Format arguments
  */
-#define LogInfo(...) LogInternal("INFO", 39, FLUSH_ON_INFO, __VA_ARGS__)
+#define LogInfo(...) LogInternal("INFO", LOGGER_INFO_COLOR, FLUSH_ON_INFO, __VA_ARGS__)
 
 #ifdef BUILDSTYLE_DEBUG
 /**
  * Log an info message, but only in debug builds
  * @param ... Format arguments
  */
-#define LogDebug(...) LogInternal("DEBUG", 39, FLUSH_ON_DEBUG, __VA_ARGS__)
+#define LogDebug(...) LogInternal("DEBUG", LOGGER_DEBUG_COLOR, FLUSH_ON_DEBUG, __VA_ARGS__)
 #else
 #define LogDebug(...)
 #endif
@@ -52,12 +65,12 @@ void LogInternal(const char *type, int color, bool flush, const char *message, .
  * Log a warning message
  * @param ... Format arguments
  */
-#define LogWarning(...) LogInternal("WARN", 33, FLUSH_ON_WARNING, __VA_ARGS__)
+#define LogWarning(...) LogInternal("WARN", LOGGER_WARNING_COLOR, FLUSH_ON_WARNING, __VA_ARGS__)
 
 /**
  * Log an error message
  * @param ... Format arguments
  */
-#define LogError(...) LogInternal("ERROR", 31, FLUSH_ON_ERROR, __VA_ARGS__)
+#define LogError(...) LogInternal("ERROR", LOGGER_ERROR_COLOR, FLUSH_ON_ERROR, __VA_ARGS__)
 
 #endif //GAME_LOGGING_H
