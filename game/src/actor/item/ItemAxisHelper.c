09088d2eec3894b13b91e49929df0b8aa6e79e52
droc101 <37421449+droc101@users.noreply.github.com>
2026-09-22 16:16:17 -0500

    fix compile errors from merge

--- game/src/actor/item/ItemAxisHelper.c
+++ game/src/actor/item/ItemAxisHelper.c
@@ -79,13 +79,8 @@ static void ItemEraserInteracted(Actor *this)
 }
 
 ActorDefinition itemAxisHelperActorDefinition = {
-	.Update = DefaultActorUpdate,
 	.OnPlayerContactAdded = ItemEraserOnPlayerContactAdded,
-	.OnPlayerContactPersisted = DefaultActorOnPlayerContactPersisted,
-	.OnPlayerContactRemoved = DefaultActorOnPlayerContactRemoved,
-	.RenderUi = DefaultActorRenderUi,
 	.Interact = ItemEraserInteracted,
-	.Destroy = DefaultActorDestroy,
 	.Init = ItemAxisHelperInit,
 };
 
